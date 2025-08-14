88a2719c2ab0968ae769a86860f0efc1f418ef9c
kzx0701 <839711518@qq.com>
2025-08-14 17:59:55 +0800

    更新登录页、首页

---
+++ pages/home/index.vue
@@ -0,0 +1,23 @@
+<!-- 首页 -->
+<template>
+	<view class="home-container">
+		<view class="content-container"></view>
+	</view>
+</template>
+
+<script></script>
+
+<style scoped lang="scss">
+.home-container {
+	background-color: #8d9f5e;
+	height: 100vh;
+	.content-container {
+		background-color: #ecebeb;
+		position: fixed;
+		bottom: 0;
+		height: 80%;
+		width: 100%;
+		border-radius: 50rpx 50rpx 0 0;
+	}
+}
+</style>
--- pages/index/login.vue
+++ pages/index/login.vue
@@ -1,23 +1,41 @@
 <!-- 登陆页 -->
 <template>
 	<view class="login-container">
+		<view class="title-container">
+			<view>欢迎来到</view>
+			<view>萌宠之家</view>
+		</view>
 		<view class="login-section">
 			<image class="login-bg-pet" src="/static/login_bg.png" mode="widthFix"></image>
+			<view class="action-container">
+				<button class="login-btn" open-type="getPhoneNumber" @getphonenumber="handleGetPhoneNumber">一键登录</button>
+				<button class="skip-btn" @click="switchTab('/pages/home/index')">跳过</button>
+			</view>
 		</view>
 	</view>
 </template>
 
-<script></script>
+<script setup>
+// 一键登录
+const handleGetPhoneNumber = (e) => {
+	console.log(e);
+};
+// 页面跳转
+const switchTab = (url) => {
+	uni.switchTab({
+		url: url
+	});
+};
+</script>
 
 <style scoped lang="scss">
 .login-container {
 	background-color: #8d9f5e;
 	height: 100vh;
-	.login-bg-pet {
-		width: 350rpx;
-		position: absolute;
-		top: -255rpx;
-		right: 20rpx;
+	.title-container {
+		color: #ffffff;
+		font-size: 50rpx;
+		padding: 80rpx;
 	}
 	.login-section {
 		background-color: #f9f9f9;
@@ -26,7 +44,41 @@
 		position: absolute;
 		bottom: 0;
 		border-radius: 50rpx 50rpx 0 0;
-		border-top: 5rpx solid #534104;
+		border-top: 5rpx solid #000000;
+		.login-bg-pet {
+			width: 500rpx;
+			position: absolute;
+			top: -360rpx;
+			right: 20rpx;
+		}
+		.action-container {
+			margin-top: 40%;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			gap: 40rpx;
+			.login-btn {
+				width: 80%;
+				height: 80rpx;
+				background-color: #8d9f5e;
+				border-radius: 40rpx;
+				color: #fff;
+				font-size: 30rpx;
+				font-weight: 500;
+				margin: 0 auto;
+				border: 0px;
+			}
+			.skip-btn {
+				width: 80%;
+				height: 80rpx;
+				background-color: #e3e3e3;
+				border-radius: 40rpx;
+				color: #959595;
+				font-size: 30rpx;
+				font-weight: 500;
+				border: 0px;
+			}
+		}
 	}
 }
 </style>
