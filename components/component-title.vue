223c52b5b6c50cc1649857fe2936902f09918639
kzx0701 <839711518@qq.com>
2025-08-15 18:00:22 +0800

    更新首页

---
+++ components/component-title.vue
@@ -0,0 +1,32 @@
+<!-- 组件标题 -->
+<template>
+	<view class="component-container">
+		<view class="title">{{ title }}</view>
+		<view class="more">更多</view>
+	</view>
+</template>
+
+<script setup>
+const props = defineProps({
+	title: {
+		type: String,
+		default: '默认标题'
+	}
+});
+</script>
+
+<style scoped lang="scss">
+.component-container {
+	display: flex;
+	justify-content: space-between;
+	align-items: baseline;
+	padding: 20rpx 0;
+	.title {
+		font-size: 36rpx;
+	}
+	.more {
+		font-size: 26rpx;
+		color: #999;
+	}
+}
+</style>
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -2,20 +2,47 @@
 <template>
 	<view class="home-container">
 		<view class="top-container">
+			<slideMenuVue></slideMenuVue>
 			<view class="search-container">
 				<uv-input
-					placeholder="请输入内容"
+					placeholder="查询宠物信息"
 					border="none"
+					prefixIcon="search"
+					prefixIconStyle="font-size: 26px;color: #909399"
 					:clearable="true"
 					:customStyle="{ backgroundColor: '#ecebeb', padding: '10rpx', borderRadius: '15rpx' }"
 				></uv-input>
 			</view>
 		</view>
-		<view class="content-container"></view>
+		<view class="content-container">
+			<!-- 最受欢迎宠物 -->
+			<view class="popular-container">
+				<componentTitleVue title="最受欢迎"></componentTitleVue>
+				<scroll-view class="scroll-container" :scroll-x="true">
+					<view class="scroll-item" v-for="(item, index) in 4" :key="index">
+						<image class="scroll-bg" src="/static/pet.png" mode="aspectFill"></image>
+						<view class="mask"></view>
+						<view class="scroll-content">
+							<view class="name-container">
+								<view class="name">大黑</view>
+								<uv-icon name="man" color="#8bbdec" size="22"></uv-icon>
+							</view>
+							<view class="location-container">
+								<uv-icon name="map-fill" color="#ffffff" size="12"></uv-icon>
+								<view class="location">徐州市</view>
+							</view>
+						</view>
+					</view>
+				</scroll-view>
+			</view>
+		</view>
 	</view>
 </template>
 
-<script setup></script>
+<script setup>
+import componentTitleVue from '../../components/component-title.vue';
+import slideMenuVue from '../../components/slide-menu.vue';
+</script>
 
 <style scoped lang="scss">
 .home-container {
@@ -38,8 +65,59 @@
 		position: absolute;
 		bottom: 0;
 		height: 80%;
-		width: 100%;
-		border-radius: 80rpx 80rpx 0 0;
+		width: -webkit-fill-available;
+		border-radius: 60rpx 60rpx 0 0;
+		padding: 0 30rpx;
+		.popular-container {
+			.scroll-container {
+				while: 100%;
+				white-space: nowrap;
+				.scroll-item {
+					display: inline-block;
+					align-items: center;
+					position: relative;
+					width: 30%;
+					height: 300rpx;
+					border-radius: 20rpx;
+					margin-right: 10rpx;
+					overflow: hidden;
+					.scroll-bg {
+						width: 100%;
+						height: 100%;
+					}
+					.mask {
+						position: absolute;
+						left: 0;
+						top: 0;
+						width: 100%;
+						height: 100%;
+						background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, /* 顶部完全透明 */ rgba(0, 0, 0, 0.3) 50%, /* 中间半透明 */ rgba(0, 0, 0, 0.8) 100% /* 底部深色 */);
+						z-index: 1;
+					}
+					.scroll-content {
+						position: absolute;
+						bottom: 0;
+						color: #ffffff;
+						padding: 20rpx;
+						z-index: 2;
+						.name-container {
+							display: flex;
+							flex-direction: row;
+							align-items: center;
+							font-size: 24rpx;
+							gap: 10rpx;
+						}
+						.location-container {
+							display: flex;
+							flex-direction: row;
+							align-items: baseline;
+							font-size: 20rpx;
+							gap: 10rpx;
+						}
+					}
+				}
+			}
+		}
 	}
 }
 </style>
